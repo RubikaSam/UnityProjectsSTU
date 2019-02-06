86aa7bf5fbd208ed2d2baa86814082498f740825
SamuelD <workflow.samuel@gmail.com>
2019-02-06 11:53:28 +0100

    Added SoundProjectEMP
    AudioMixer exercise EMP

---
+++ SoundProjectEMP/Assets/STUScripts/MixerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MixerScript : MonoBehaviour
+{
+
+    public AudioMixer audioMix;
+    public AudioMixerSnapshot snap100;
+    public AudioMixerSnapshot snap50;
+
+    public Slider volumeSlider;
+
+    bool bPause = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Pause()
+    {
+        bPause = !bPause;
+        if (bPause)
+        {
+            snap50.TransitionTo(0.5f);
+        }
+        else
+        {
+            snap100.TransitionTo(0.5f);
+        }
+    }
+
+    public void SetVolume()
+    {
+        audioMix.SetFloat("MasterVolume", volumeSlider.value);
+    }
+}
